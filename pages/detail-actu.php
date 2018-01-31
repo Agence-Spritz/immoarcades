6cf160848ac2c171666e638439c630c51a2f9913
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien-2.local>
2018-01-31 18:22:58 +0100

    Finalisation

--- pages/detail-actu.php
+++ pages/detail-actu.php
@@ -1,29 +1,24 @@
 <?php	// Requête pour récupérer le contenu de la page concernée
-		list($titrep, $date, $rub, $textep, $texte2p) = mysqli_fetch_array(mysqli_query($link, "SELECT titre, dbu, rub, texte, texte2 FROM ".$table_prefix."_pages WHERE page='actu' AND ID='$id' "));
-		
+		list($titrep, $date, $rub, $textep, $texte2p) = mysqli_fetch_array(mysqli_query($link, "SELECT titre, dbu, rub, texte, texte2 FROM ".$table_prefix."_pages WHERE page='actu' AND ID='$id' "));		
 ?>
 
 <div id="main">
 	
 	<!-- Page en-tête
 	================================================== -->
-	<div class="section section-bg-2 section-fixed pt-6 pb-3">
+	<div class="section section-bg-generique section-fixed pt-14 pb-3">
 		<div class="bg-overlay-dark"></div>
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6 col-lg-6">
-					<h2 class="page-title"><?php echo $titrep; ?></h2>
-					<div class="breadcrumb mt-1 pl-0">
-						<ul class="breadcrumbs">
-							<li><a href="<?php echo $defaultpg; ?>.php">Accueil</a></li>
-							<li><?php echo $titrep; ?></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-6">
-					<div class="text-right">
-						<div class="page-title-subtext extra-font">
-							
+				<div class="col-sm-12">
+					<div class="text-center">
+						<h2 class="page-title"><?php echo $titrep; ?></h2>
+					
+						<div class="breadcrumb">
+							<ul class="breadcrumbs">
+								<li><a href="<?php echo $defaultpg; ?>.php">Accueil</a></li>
+								<li><a href="actualite-immo-hauts-de-france--69--blog" title="Voir toutes les ">blog</a></li>
+							</ul>
 						</div>
 					</div>
 				</div>
@@ -35,7 +30,7 @@
 
 	<!-- Corps de la page
 	================================================== -->	
-	<div class="section pt-11 pb-9">
+	<div class="section pt-8 pb-8">
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -47,37 +42,39 @@
 									<?php if (is_file('./images/pages-immobilier-tourcoing-lys-les-lannoy/'.$id.'.jpg')) { ?>
 									
 										<?php if($texte2p) {?>
-											<a href="<?php echo $texte2p; ?>" target="_blank">
+											<a href="<?php echo $texte2p; ?>" title="En savoir plus sur cette actualité" target="_blank">
 										<?php } ?>
 									
 										<img src="<?php echo './images/pages-immobilier-tourcoing-lys-les-lannoy/'.$id.'.jpg'; ?>" alt="<?php echo $titrep; ?>" title="<?php echo $titrep; ?>" />
 									<?php } else { ?>
-										<img src="images/blog/blog_1140x590.jpg" alt="Actualité Jorion Desmet" />
+										<img src="images/blog/fond-agence.jpg" alt="Actualité Immobilière des Arcades, Tourcoing, Lys-les-Lannoy" />
 									<?php } ?>
 									
 										<?php if($texte2p) {?>
 											</a>
 										<?php } ?>
 									
-									<span class="entry-categories">
-										<a href="agence-immobiliere-region-wallone--69--actu-le-saviez-vous" data-filter=".<?php echo slugify($rub); ?>"><?php echo $rub; ?></a>
-									</span>
 								</div>
 								<div class="entry-content pl-0 pr-0">
 									<div class="entry-header">
 										<h3 class="entry-title">
 											<?php echo $titrep; ?>
 										</h3>
-										<ul class="entry-meta">
-											<li><?php echo date_fr($date); ?></li>
-										</ul>
 									</div>
 									<div class="content">
 										<?php echo $textep; ?>
 									</div>
 									<?php if($texte2p) {?>
 									<div class="entry-footer">
-										<a class="readmore" href="<?php echo $texte2p; ?>" target="_blank">En savoir plus</a>
+										
+										
+										<div class="mb-2">
+											<a class="btn autosize btn-primary mb-2" href="<?php echo $texte2p; ?>">En savoir plus</a>
+										</div>
+										<div class="">
+										<p>source : <?php echo $texte2p; ?></p>
+										</div>
+							
 									</div>
 									<?php } ?>
 								</div>
