930c5d4612d2efad02f362e0832ebd63c8a7f962
Sébastien PAQUET <cavallinox@MacBook-Pro-de-Sebastien-2.local>
2018-03-28 23:21:15 +0200

    Modif import photos

--- admin/dezip.php
+++ admin/dezip.php
@@ -139,19 +139,151 @@ $erreur = 0;
 		
 		$nouveaute = 1;
 		
-	
-		// On crée les variables des images
-		$p=0;
+		// On récupére les photos
 		$set_image = $bien->images;
 		$image_path = base_url().'kaio';
 		
-		foreach ($set_image->image as $image) {
-			$compteur = sprintf("%02d", $p);
-			
-			${'PHOTO_' . $compteur} = $image_path.'/'.$image;
-			$p++;
-		
+		if($set_image->image[0]) {
+			$photo_01 = $image_path."/".$set_image->image[0];
+		} else {
+			$photo_01 = NULL;
 		}
+		
+		if($set_image->image[1]) {
+			$photo_02 = $image_path."/".$set_image->image[1];
+		} else {
+			$photo_02 = NULL;
+		}
+		
+		if($set_image->image[2]) {
+			$photo_03 = $image_path."/".$set_image->image[2];
+		} else {
+			$photo_03 = NULL;
+		}
+		
+		if($set_image->image[3]) {
+			$photo_04 = $image_path."/".$set_image->image[3];
+		} else {
+			$photo_04 = NULL;
+		}
+		
+		if($set_image->image[4]) {
+			$photo_05 = $image_path."/".$set_image->image[4];
+		} else {
+			$photo_05 = NULL;
+		}
+		
+		if($set_image->image[5]) {
+			$photo_06 = $image_path."/".$set_image->image[5];
+		} else {
+			$photo_06 = NULL;
+		}
+		
+		if($set_image->image[6]) {
+			$photo_07 = $image_path."/".$set_image->image[6];
+		} else {
+			$photo_07 = NULL;
+		}
+		
+		if($set_image->image[7]) {
+			$photo_08 = $image_path."/".$set_image->image[7];
+		} else {
+			$photo_08 = NULL;
+		}
+		
+		if($set_image->image[8]) {
+			$photo_09 = $image_path."/".$set_image->image[8];
+		} else {
+			$photo_09 = NULL;
+		}
+		
+		if($set_image->image[9]) {
+			$photo_10 = $image_path."/".$set_image->image[9];
+		} else {
+			$photo_10 = NULL;
+		}
+		
+		if($set_image->image[10]) {
+			$photo_11 = $image_path."/".$set_image->image[10];
+		} else {
+			$photo_11 = NULL;
+		}
+		
+		if($set_image->image[11]) {
+			$photo_12 = $image_path."/".$set_image->image[11];
+		} else {
+			$photo_12 = NULL;
+		}
+		
+		if($set_image->image[12]) {
+			$photo_13 = $image_path."/".$set_image->image[12];
+		} else {
+			$photo_13 = NULL;
+		}
+		
+		if($set_image->image[13]) {
+			$photo_14 = $image_path."/".$set_image->image[13];
+		} else {
+			$photo_14 = NULL;
+		}
+		
+		if($set_image->image[14]) {
+			$photo_15 = $image_path."/".$set_image->image[14];
+		} else {
+			$photo_15 = NULL;
+		}
+		
+		if($set_image->image[15]) {
+			$photo_16 = $image_path."/".$set_image->image[15];
+		} else {
+			$photo_16 = NULL;
+		}
+		
+		if($set_image->image[16]) {
+			$photo_17 = $image_path."/".$set_image->image[16];
+		} else {
+			$photo_17 = NULL;
+		}
+		
+		if($set_image->image[17]) {
+			$photo_18 = $image_path."/".$set_image->image[17];
+		} else {
+			$photo_18 = NULL;
+		}
+		
+		if($set_image->image[18]) {
+			$photo_19 = $image_path."/".$set_image->image[18];
+		} else {
+			$photo_19 = NULL;
+		}
+		
+		if($set_image->image[19]) {
+			$photo_20 = $image_path."/".$set_image->image[19];
+		} else {
+			$photo_20 = NULL;
+		}
+		
+		
+	
+		// On crée les variables des images
+		//$p=1;
+	
+		
+/*
+		foreach ($set_image->image as $image) {
+			//$compteur = sprintf("%02d", $p);
+
+			$liste_colonnes .= "`PHOTO_".$p."`,";
+			
+			$liste_photos .= "'".${"PHOTO_".$p} = $image_path."/".$image."',";
+			
+			
+			$p++;
+		}
+*/
+		
+		
+		
 		
 // 3 ON REMPLIT LA TABLE
 		if ($n>0) {
@@ -254,26 +386,26 @@ $erreur = 0;
 			'".$type."',
 			'".$dbu."',
 			'".$dmod."',
-			'".$PHOTO_01."',
-			'".$PHOTO_02."',
-			'".$PHOTO_03."',
-			'".$PHOTO_04."',
-			'".$PHOTO_05."',
-			'".$PHOTO_06."',
-			'".$PHOTO_07."',
-			'".$PHOTO_08."',
-			'".$PHOTO_09."',
-			'".$PHOTO_10."',
-			'".$PHOTO_11."',
-			'".$PHOTO_12."',
-			'".$PHOTO_13."',
-			'".$PHOTO_14."',
-			'".$PHOTO_15."',
-			'".$PHOTO_16."',
-			'".$PHOTO_17."',
-			'".$PHOTO_18."',
-			'".$PHOTO_19."',
-			'".$PHOTO_20."',
+			'".$photo_01."',
+			'".$photo_02."',
+			'".$photo_03."',
+			'".$photo_04."',
+			'".$photo_05."',
+			'".$photo_06."',
+			'".$photo_07."',
+			'".$photo_08."',
+			'".$photo_09."',
+			'".$photo_10."',
+			'".$photo_11."',
+			'".$photo_12."',
+			'".$photo_13."',
+			'".$photo_14."',
+			'".$photo_15."',
+			'".$photo_16."',
+			'".$photo_17."',
+			'".$photo_18."',
+			'".$photo_19."',
+			'".$photo_20."',
 			'".$prix."',
 			'".$prix_fai."',
 			'".$cacherprix."',
@@ -305,6 +437,8 @@ $erreur = 0;
 			");	
 			
 			
+			
+						
 			
 			echo $n." : Enregistrement du bien réf.: ".$ref."<br />";
 			
